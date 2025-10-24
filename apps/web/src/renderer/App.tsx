31103466aba2adfe43c3c55cb9f65b4a5142bcc2
Edivar Cruz <edivarfilho16@gmail.com>
2025-10-24 09:55:48 -0300

    fix(web): redireciona a rota rai para login e porta do back estabelecida

--- apps/web/src/renderer/App.tsx
+++ apps/web/src/renderer/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import React from 'react';
 import LoginPage from './pages/Login';
 import RecoveryPage from './pages/Recovery';
@@ -10,6 +10,7 @@ export function App() {
     <BrowserRouter>
       <ToastContainer />
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/recuperar-senha" element={<RecoveryPage />} />
       </Routes>
