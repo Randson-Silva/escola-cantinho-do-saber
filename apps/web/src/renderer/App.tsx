5ae72c5db2d867d6d7708a9fc8ee4406384eaf2f
Edivar Cruz <edivarfilho16@gmail.com>
2025-10-23 22:50:27 -0300

    feat(router): configurar rotas /login e /recuperar-senha

--- apps/web/src/renderer/App.tsx
+++ apps/web/src/renderer/App.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import LoginPage from './pages/Login';
+import './global.css';
+import RecoveryPage from './pages/Recovery';
 
 export function App() {
-  return <h1>Cantinho do saber</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} /> {/* Rota para a página de login */}
+        <Route path="/recuperar-senha" element={<RecoveryPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
